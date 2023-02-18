c28dff6c62aa4cbaa3cd243d872261b6fc2e7031
itpark <a.m.smyslov@yandex.ru>
2023-02-18 18:53:39 +0300

    111

---
+++ LonglongNumberConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using LonglongNumbersLibrary;
+
+LongNumber longNumber1 = new LongNumber("1");
+LongNumber longNumber2 = new LongNumber("1");
+LongNumber longNumberSum = longNumber1 + longNumber2;
+
+Console.WriteLine($"longNumber1: {longNumber1}");
+Console.WriteLine($"longNumber2: {longNumber2}");
+Console.WriteLine($"longNumberSum: {longNumberSum}");
+
+if (longNumber1 >= longNumber2)
+{
+    Console.WriteLine("longNumber1 больше либо равно longNumber2");
+}
+else
+{
+    Console.WriteLine("longNumber1 НЕ больше  либо равно longNumber2");
+}
